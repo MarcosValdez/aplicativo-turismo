c5f11dd5576a753c914a2eca26d91cbd676d4b6f
MarcosValdez <marcos.valdez333@hotmail.com>
2022-06-04 00:23:59 -0500

    style: continente view y pais view

---
+++ lib/pais.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class pais extends StatefulWidget{
+  @override
+  _paisState createState() => _paisState();
+}
+
+class _paisState extends State<pais> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Destinos a visitar"),
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _boton("Perú"),
+                _boton("Brazil"),
+                _boton("Argentina")
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boton(String texto) {
+    return Container(
+        height: 90,
+        width: 200,
+        margin: EdgeInsets.only(top:50, bottom: 40),
+        child: RaisedButton(
+          child:Text(texto, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0),),
+          color: Colors.blue,
+          elevation: 5,
+          padding: EdgeInsets.all(30),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0)
+          ),
+          onPressed: ()=>{
+            // use pageroute para lanzar a otra ventana
+            Navigator.pop(context),
+            print("BOTON: " +texto)
+          },
+        )
+    );
+  }
+}
