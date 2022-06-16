7122521e42ad36832429d845c941661d495d9a8e
EOrtega27 <ortegachaveznsm@gmail.com>
2022-06-16 16:04:32 -0500

    vertical carousel

--- lib/screens/principal.dart
+++ lib/screens/principal.dart
@@ -28,97 +28,97 @@ class _principalState extends State<Principal> {
         child: Column(
           //padding: const EdgeInsets.all(10.0),
           children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 520,
-              scrollDirection: Axis.vertical
-            ),
-            items: Images
-                .map((i) {
-              return Builder(builder: (BuildContext context) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  child: SingleChildScrollView(
-                    child: Center(
-                      child: Column(
-                        children: [
-                          if (i == 'assets/img1.jpg')
-                            Text(
-                              "${Places[0]}",
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w800,
-                                color: Colors.white,
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 520,
+                scrollDirection: Axis.vertical
+              ),
+              items: Images
+                  .map((i) {
+                return Builder(builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: SingleChildScrollView(
+                      child: Center(
+                        child: Column(
+                          children: [
+                            if (i == 'assets/img1.jpg')
+                              Text(
+                                "${Places[0]}",
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          if (i == 'assets/img2.jpg')
-                            Text(
-                              "${Places[1]}",
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w800,
-                                color: Colors.white,
+                            if (i == 'assets/img2.jpg')
+                              Text(
+                                "${Places[1]}",
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          if (i == 'assets/img3.jpg')
-                            Text(
-                              "${Places[2]}",
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w800,
-                                color: Colors.white,
+                            if (i == 'assets/img3.jpg')
+                              Text(
+                                "${Places[2]}",
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white,
+                                ),
                               ),
+                            Image.asset(
+                                i,
+                              height: 270,
                             ),
-                          Image.asset(
-                              i,
-                            height: 270,
-                          ),
-                          SizedBox(height: 20),
-                          if (i == 'assets/img1.jpg')
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 25.0, right: 25.0),
-                              child: Text("${Desc[0]}",
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.white,
-                                  ),
-                                  textAlign: TextAlign.center),
-                            ),
-                          if (i == 'assets/img2.jpg')
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 25.0, right: 25.0),
-                              child: Text("${Desc[1]}",
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.white,
-                                  ),
-                                  textAlign: TextAlign.center),
-                            ),
-                          if (i == 'assets/img3.jpg')
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 25.0, right: 25.0),
-                              child: Text("${Desc[2]}",
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.white,
-                                  ),
-                                  textAlign: TextAlign.center),
-                            ),
-                        ],
+                            SizedBox(height: 20),
+                            if (i == 'assets/img1.jpg')
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 25.0, right: 25.0),
+                                child: Text("${Desc[0]}",
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w800,
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center),
+                              ),
+                            if (i == 'assets/img2.jpg')
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 25.0, right: 25.0),
+                                child: Text("${Desc[1]}",
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w800,
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center),
+                              ),
+                            if (i == 'assets/img3.jpg')
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 25.0, right: 25.0),
+                                child: Text("${Desc[2]}",
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w800,
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center),
+                              ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              });
-            }).toList(),
-          ),
+                  );
+                });
+              }).toList(),
+            ),
             /*Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: Images.map((urlOfItem) {
